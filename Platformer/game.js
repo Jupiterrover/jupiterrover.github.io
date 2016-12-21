c8b37f2415465ea2acb165f987fe2b351a014273
Tyler Figueroa <tylerhockeyguy11@gmail.com>
2016-12-21 21:55:42 +0000

    PROVERSOIN

--- Platformer/game.js
+++ Platformer/game.js
@@ -3,6 +3,7 @@ var GROUND_SPRITE_WIDTH = 50;
 var GROUND_SPRITE_HEIGHT = 50;
 var GRAVITY = 0.3;
 var JUMP = -5;
+var DOWNJUMP = 5;
 var numGroundSprites;
 var player;
 var obstacleSprites;
@@ -12,112 +13,140 @@ var score;
 
 
 
-function setup(){
-    
-    score = 0;
-    
-    isGameOver = false;
-    createCanvas(500, 380);
-    
-    background(150, 200, 250);
-    
+function setup() {
 
-    groundSprites = new Group();   
-    obstacleSprites = new Group();
 
-  
-    numGroundSprites = width/GROUND_SPRITE_WIDTH + 1;
-    
-    
-    
-    for (var n = 0; n < numGroundSprites; n++) {
-    var groundSprite = createSprite(n*50, height-25, GROUND_SPRITE_WIDTH, GROUND_SPRITE_HEIGHT);
+  score = 0;
+
+  isGameOver = false;
+  createCanvas(1366, 700);
+
+  // background(30, 0, 0);
+
+
+  groundSprites = new Group();
+  obstacleSprites = new Group();
+
+
+  numGroundSprites = width / GROUND_SPRITE_WIDTH + 1;
+
+
+
+  for (var n = 0; n < numGroundSprites; n++) {
+    var groundSprite = createSprite(n * 50, height - 25, GROUND_SPRITE_WIDTH, GROUND_SPRITE_HEIGHT);
     groundSprites.add(groundSprite);
   }
-  player = createSprite(100, height-75, 50, 50);
+  player = createSprite(100, height - 75, 50, 50);
 }
 
 function endGame() {
   isGameOver = true;
 }
 
-function draw(){
-    score = score + 1;
+function draw() {
 
-     if (isGameOver) {
-    background(0);
-    fill(255);
+  obstacleSprites.overlap(player, endGame);
+  textAlign(CENTER);
+  
+
+
+  if (isGameOver) {
+    background(20,0,0);
+    // fill(255);
     textAlign(CENTER);
     //text("Game Over! Click anywhere to restart", camera.position.x, camera.position.y);
     text("Your score was: " + score, camera.position.x, camera.position.y - 20);
     text("Game Over! Click anywhere to restart", camera.position.x, camera.position.y);
+    player.velocity.y = 0;
+    player.velocity.x = 0;
 
-  } else {
-      
-    
-    background(150, 200, 250);
-
-    camera.position.x = player.position.x + (width/4);
-    
-    player.position.x = player.position.x + 5;
-    
-    var firstGroundSprite = groundSprites[0];
-
-    if (firstGroundSprite.position.x <= camera.position.x - (width/2 + firstGroundSprite.width/2)) {
-        groundSprites.remove(firstGroundSprite);
-        firstGroundSprite.position.x = firstGroundSprite.position.x + numGroundSprites*firstGroundSprite.width;
-        groundSprites.add(firstGroundSprite);
-        
-         
-        }
+  }
+  else {
     player.velocity.y = player.velocity.y + GRAVITY;
 
-if (groundSprites.overlap(player)) {
-  player.velocity.y = 0;
-  player.position.y = (height-50) - (player.height/2);
-}
 
-if (keyDown(UP_ARROW)) {
-  player.velocity.y = JUMP;
-}
+
+
+
+
+    background(20, 0, 0);
+
+    camera.position.x = player.position.x + (width / 4);
+
+    player.position.x = player.position.x + 5;
+
+    var firstGroundSprite = groundSprites[0];
+
+    text(score, camera.position.x, 10);
+    score = score + 1;
+
+    if (firstGroundSprite.position.x <= camera.position.x - (width / 2 + firstGroundSprite.width / 2)) {
+      groundSprites.remove(firstGroundSprite);
+      firstGroundSprite.position.x = firstGroundSprite.position.x + numGroundSprites * firstGroundSprite.width;
+      groundSprites.add(firstGroundSprite);
+
+
+    }
+
+    if (groundSprites.overlap(player)) {
+      player.velocity.y = 0;
+      player.position.y = (height - 50) - (player.height / 2);
+    }
+
+    if (keyDown(UP_ARROW)) {
+      player.velocity.y = JUMP;
+    }
+    else if (keyDown(DOWN_ARROW)) {
+      player.velocity.y = DOWNJUMP;
+    }
+    else if (keyDown(RIGHT_ARROW) && player.velocity.x < 20) {
+      player.velocity.x = player.velocity.x + .5;
+    }
+    else if (keyDown(LEFT_ARROW) && player.velocity.x > .5) {
+      player.velocity.x = player.velocity.x - .5;
+    }
+
 
 
     if (random() > 0.95) {
-        var obstacle = createSprite(camera.position.x + width, (height-50) - 15, 30, 30);
-        obstacleSprites.add(obstacle);
-        }
+      //var obstacle = createSprite(camera.position.x + width, (height - 50) - 15, 30, 30);
+      var obstacle = createSprite(camera.position.x + width, random(0, (height - 50) - 15), 30, 30);
+
+      obstacleSprites.add(obstacle);
+    }
     var firstObstacle = obstacleSprites[0];
-    if (obstacleSprites.length > 0 && firstObstacle.position.x <= camera.position.x - (width/2 + firstObstacle.width/2)) {
-        removeSprite(firstObstacle);
-        }
+    if (obstacleSprites.length > 0 && firstObstacle.position.x <= camera.position.x - (width / 2 + firstObstacle.width / 2)) {
+      removeSprite(firstObstacle);
+    }
 
 
-    obstacleSprites.overlap(player, endGame);
-    textAlign(CENTER);
-    text(score, camera.position.x, 10);
 
 
-    
+
+
 
   }
-    
-    drawSprites();
-    
-    
+  if (player.position.y < 40){
+    endGame();
+  }
+
+  drawSprites();
+
+
 }
 
 function mouseClicked() {
   if (isGameOver) {
-      score = 0;
+    score = 0;
 
 
     for (var n = 0; n < numGroundSprites; n++) {
       var groundSprite = groundSprites[n];
-      groundSprite.position.x = n*50;
+      groundSprite.position.x = n * 50;
     }
 
     player.position.x = 100;
-    player.position.y = height-75;
+    player.position.y = height - 75;
 
     obstacleSprites.removeSprites();
 
